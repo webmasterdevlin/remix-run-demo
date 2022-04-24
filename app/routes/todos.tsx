a70bc365d8ecc27e95e5f644e729ca7ae5a35029
Devlin Duldulao <webmasterdevlin@gmail.com>
2022-04-24 09:57:33 +0200

    update

--- app/routes/todos.tsx
+++ app/routes/todos.tsx
@@ -1,12 +1,36 @@
 import { db } from "~/utils/db.server";
-import { useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData, useTransition } from "@remix-run/react";
 import type { Todo } from "@prisma/client";
-import useLog from "~/hooks/useLog";
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
+import { useEffect, useRef } from "react";
 
-export const loader = async () => await db.todo.findMany();
+export const loader: LoaderFunction = async () => await db.todo.findMany();
+
+export const action: ActionFunction = async (args) => {
+  const formData = await args.request.formData();
+  const title = formData.get("title") as string;
+
+  return await db.todo.create({
+    data: {
+      title: title,
+      completed: false,
+    },
+  });
+};
 
 export default function Todos() {
   const todos = useLoaderData<Todo[]>();
+  const transition = useTransition();
+
+  const titleRef = useRef<any>();
+
+  const isAdding = transition.state === "submitting";
+
+  useEffect(() => {
+    if (isAdding) {
+      titleRef.current?.reset();
+    }
+  }, [isAdding]);
 
   return (
     <>
@@ -15,16 +39,20 @@ export default function Todos() {
       </div>
       <ul>
         {todos.map((t) => {
-          const time = t.completed ? null : new Date(t.createdAt);
           return (
             <div key={t.id}>
-              <h2>
-                {t.title} {t.completed ? "✅" : `❌ - ${time?.toDateString()}`}
-              </h2>
+              <h2>{t.title}</h2>
             </div>
           );
         })}
       </ul>
+      <Form method="post">
+        <div>
+          <label htmlFor="title">Title</label>
+        </div>
+        <input id="title" name="title" type="text" ref={titleRef} />
+        <button type="submit">Add</button>
+      </Form>
     </>
   );
 }
