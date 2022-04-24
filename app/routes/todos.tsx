e7ebdd65238bed98f33c20e315c839d07c680555
Devlin Duldulao <webmasterdevlin@gmail.com>
2022-04-24 15:39:02 +0200

    update

--- app/routes/todos.tsx
+++ app/routes/todos.tsx
@@ -1,3 +1,9 @@
+/*
+ * Try to disable JavaScript of the browser and do reload to see
+ * full server render. Proof is no fetch request in the network tab of chrome devtools. It is embedded in the HTML.
+ * This is a fast server render.
+ * */
+
 import { db } from "~/utils/db.server";
 import { Form, useLoaderData, useTransition } from "@remix-run/react";
 import type { Todo } from "@prisma/client";
@@ -8,23 +14,33 @@ export const loader: LoaderFunction = async () => await db.todo.findMany();
 
 export const action: ActionFunction = async (args) => {
   const formData = await args.request.formData();
+  const _action = formData.get("_action");
   const title = formData.get("title") as string;
 
-  return await db.todo.create({
-    data: {
-      title: title,
-      completed: false,
-    },
-  });
+  switch (_action) {
+    case "create":
+      // this can be put in another file
+      return await db.todo.create({
+        data: {
+          title: title,
+          completed: false,
+        },
+      });
+
+    case "delete":
+      const id = formData.get("id") as string;
+      return await db.todo.delete({ where: { id } });
+  }
 };
 
 export default function Todos() {
   const todos = useLoaderData<Todo[]>();
-  const transition = useTransition();
+  const { state, submission } = useTransition();
 
   const titleRef = useRef<any>();
 
-  const isAdding = transition.state === "submitting";
+  const isAdding =
+    state === "submitting" && submission.formData.get("_action") === "create";
 
   useEffect(() => {
     if (isAdding) {
@@ -37,22 +53,35 @@ export default function Todos() {
       <div>
         <h1>What's Next?</h1>
       </div>
-      <ul>
-        {todos.map((t) => {
-          return (
-            <div key={t.id}>
-              <h2>{t.title}</h2>
-            </div>
-          );
-        })}
-      </ul>
-      <Form method="post">
+      {/*does revalidation after the post to sync data with the UI*/}
+      <Form method="post" reloadDocument>
         <div>
           <label htmlFor="title">Title</label>
         </div>
         <input id="title" name="title" type="text" ref={titleRef} />
-        <button type="submit">Add</button>
+        <button disabled={isAdding} type="submit" name="_action" value="create">
+          {isAdding ? "Adding" : "Create"}
+        </button>
       </Form>
+      <ul>
+        {/*declarative mutation =)*/}
+        {todos.map((t) => {
+          return (
+            <Form
+              method="post"
+              key={t.id}
+              reloadDocument
+              style={{ display: "flex", gap: "1rem", alignItems: "center" }}
+            >
+              <h2>{t.title}</h2>
+              <input type="hidden" name="id" value={t.id} />
+              <button type="submit" name="_action" value="delete">
+                ✔️
+              </button>
+            </Form>
+          );
+        })}
+      </ul>
     </>
   );
 }
